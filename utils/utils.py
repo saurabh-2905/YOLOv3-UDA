correct iou calculation
Former-commit-id: 75ffd0561e53563040a15aaf9f283e13de63f821

utils/utils.py
@@ -354,25 +354,35 @@ def iou_rotated(box1, box2, x1y1x2y2=True):
                 rot_box1 = calculate_rotated(b1_cx[0], b1_cy[0], b1_w[0], b1_h[0], angle_1[0])
                 rot_box2 = calculate_rotated(b2_cx[i], b2_cy[i], b2_w[i], b2_h[i], angle_2[i])
 
-                b1_x1, b1_y1 = rot_box1.min(0)[0]
-                b1_x2, b1_y2 = rot_box1.max(0)[0]
-                b2_x1, b2_y1 = rot_box2.min(0)[0]
-                b2_x2, b2_y2 = rot_box2.max(0)[0]
+                # b1_x1, b1_y1 = rot_box1.min(0)[0]
+                # b1_x2, b1_y2 = rot_box1.max(0)[0]
+                # b2_x1, b2_y1 = rot_box2.min(0)[0]
+                # b2_x2, b2_y2 = rot_box2.max(0)[0]
 
-                # get the co-ordinates of the intersection rectangle
-                inter_rect_x1 = torch.max(b1_x1, b2_x1)
-                inter_rect_y1 = torch.max(b1_y1, b2_y1)
-                inter_rect_x2 = torch.min(b1_x2, b2_x2)
-                inter_rect_y2 = torch.min(b1_y2, b2_y2)
+                # # get the co-ordinates of the intersection rectangle
+                # inter_rect_x1 = torch.max(b1_x1, b2_x1)
+                # inter_rect_y1 = torch.max(b1_y1, b2_y1)
+                # inter_rect_x2 = torch.min(b1_x2, b2_x2)
+                # inter_rect_y2 = torch.min(b1_y2, b2_y2)
+                # # Intersection area
+                # inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(
+                #     inter_rect_y2 - inter_rect_y1 + 1, min=0
+                # )
+                # # Union Area
+                # b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+                # b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+                # iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+
+                rot_box1 = Polygon( [ rot_box1[0], rot_box1[1], rot_box1[2], rot_box1[3] ] )
+                rot_box2 = Polygon( [ rot_box2[0], rot_box2[1], rot_box2[2], rot_box2[3] ] )
                 # Intersection area
-                inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(
-                    inter_rect_y2 - inter_rect_y1 + 1, min=0
-                )
+                inter_area = rot_box1.intersection(rot_box2).area
                 # Union Area
-                b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
-                b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+                union_area = rot_box1.union(rot_box2).area
 
-                iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+                iou = inter_area / (union_area + 1e-9)
 
             iou_all[i] = iou
 
@@ -391,25 +401,35 @@ def iou_rotated(box1, box2, x1y1x2y2=True):
                 rot_box1 = calculate_rotated(b1_cx[i], b1_cy[i], b1_w[i], b1_h[i], angle_1[i])
                 rot_box2 = calculate_rotated(b2_cx[i], b2_cy[i], b2_w[i], b2_h[i], angle_2[i])
 
-                b1_x1, b1_y1 = rot_box1.min(0)[0]
-                b1_x2, b1_y2 = rot_box1.max(0)[0]
-                b2_x1, b2_y1 = rot_box2.min(0)[0]
-                b2_x2, b2_y2 = rot_box2.max(0)[0]
+                # b1_x1, b1_y1 = rot_box1.min(0)[0]
+                # b1_x2, b1_y2 = rot_box1.max(0)[0]
+                # b2_x1, b2_y1 = rot_box2.min(0)[0]
+                # b2_x2, b2_y2 = rot_box2.max(0)[0]
 
-                # get the co-ordinates of the intersection rectangle
-                inter_rect_x1 = torch.max(b1_x1, b2_x1)
-                inter_rect_y1 = torch.max(b1_y1, b2_y1)
-                inter_rect_x2 = torch.min(b1_x2, b2_x2)
-                inter_rect_y2 = torch.min(b1_y2, b2_y2)
+                # # get the co-ordinates of the intersection rectangle
+                # inter_rect_x1 = torch.max(b1_x1, b2_x1)
+                # inter_rect_y1 = torch.max(b1_y1, b2_y1)
+                # inter_rect_x2 = torch.min(b1_x2, b2_x2)
+                # inter_rect_y2 = torch.min(b1_y2, b2_y2)
+                # # Intersection area
+                # inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(
+                #     inter_rect_y2 - inter_rect_y1 + 1, min=0
+                # )
+                # # Union Area
+                # b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+                # b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+                # iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+                rot_box1 = Polygon( [ rot_box1[0], rot_box1[1], rot_box1[2], rot_box1[3] ] )
+                rot_box2 = Polygon( [ rot_box2[0], rot_box2[1], rot_box2[2], rot_box2[3] ] )
                 # Intersection area
-                inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(
-                    inter_rect_y2 - inter_rect_y1 + 1, min=0
-                )
+                inter_area = rot_box1.intersection(rot_box2).area
                 # Union Area
-                b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
-                b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+                union_area = rot_box1.union(rot_box2).area
 
-                iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+                iou = inter_area / union_area
+
 
             iou_all[i] = iou
 